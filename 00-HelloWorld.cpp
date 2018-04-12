6488ee1416941c1e652447eb8fa8aa30c3614cc6
GustavoCabrera40 <38118731+GustavoCabrera40@users.noreply.github.com>
2018-04-12 17:49:00 -0300

    Create 00-HelloWorld.cpp

---
+++ 00-HelloWorld.cpp
@@ -0,0 +1,15 @@
+/*Trabajo Practico N° 00 Hello, World!
+*AED curso K1051
+*Gustavo Cabrera
+*Legajo: 160974-5
+*Objetivo: Crear un algoritmo basico que imprima en pantalla la frase Hello, Wordl!
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello, World!"<< endl;
+    
+return o;
+}
